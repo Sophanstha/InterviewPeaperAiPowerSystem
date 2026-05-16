1f1fe83bcf05b52296283a071cc602a95a9d7897
Sophanstha <sophanshrestha4@gmail.com>
2026-05-16 18:08:46 +0545

    cors

--- server/src/services/ai.service.ts
+++ server/src/services/ai.service.ts
@@ -451,7 +451,12 @@ ${jobDescription}
 const generatePDFformat = async (htmlcontent: string) => { 
   const browser = await puppeteer.launch({
   headless: true,
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+ args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',  // important on Render (limited /dev/shm)
+    '--disable-gpu',
+  ],
 });
   const page = await browser.newPage();
   await page.setContent
