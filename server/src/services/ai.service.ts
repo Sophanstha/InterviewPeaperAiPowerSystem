4c5fe972a2f53175f4b6b9ef451d7b7d427f2a19
Sophanstha <sophanshrestha4@gmail.com>
2026-05-16 17:53:34 +0545

    cors

--- server/src/services/ai.service.ts
+++ server/src/services/ai.service.ts
@@ -1,6 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
 import z from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
 import dotenv from "dotenv";
 import puppeteer from "puppeteer";
 dotenv.config();
